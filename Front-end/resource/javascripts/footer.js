c0ab78e8074c1e1407fa93765a2272909ae90e27
juangpu <juangabriel.urrutia@outlook.com>
2020-09-22 17:25:53 -0300

    Criando pasta de javascript dentro de resource adicionando o arquivo footer. js, para interação no envio do e-mail

---
+++ Front-end/resource/javascripts/footer.js
@@ -0,0 +1,15 @@
+const emailFooter = document.getElementById('emailFooter');
+
+
+// emailFooter.onclick = function () {
+//     swal ( " Bom trabalho! " , " Você clicou no botão! " , " sucesso " )
+// };
+
+let emailInput = document.getElementById('staticEmail2').value;
+
+function enviar() {
+
+    emailFooter.addEventListener("click", (event) => {event.preventDefault(); swal ( " E-mail enviado! " , ` E-mail de boas vindas enviado para ${document.getElementById('staticEmail2').value}` , "success" )} );
+}
+
+enviar()
