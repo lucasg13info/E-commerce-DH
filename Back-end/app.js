3390c304338af72f75b2c0544f93b9e66a6489ca
Lucas Estefano <lucccasestefano_@hotmail.com>
2020-10-02 20:44:05 -0300

    Ajudando a rota da index e lincando o css e img

--- Back-end/app.js
+++ Back-end/app.js
@@ -4,8 +4,8 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+
+const indexRouter = require('./routes/index')
 const carrinhoRouter = require('./routes/carrinho')
 const confirmacaoDePagamentoRouter = require('./routes/confirmacaoDePagamento')
 
@@ -21,8 +21,8 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+
+app.use('/index', indexRouter);
 app.use('/carrinho', carrinhoRouter);
 app.use('/confirmacaoDePagamento', confirmacaoDePagamentoRouter )
 
--- Back-end/routes/index.js
+++ Back-end/routes/index.js
@@ -1,9 +1,9 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const indexController = require('../controllers/indexController')
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+router.get('/', indexController.index); 
+
 
 module.exports = router;
