af4024f6e4dee32c2ee3dd860cab3b3eea8cd60d
Lucas Estefano <lucccasestefano_@hotmail.com>
2020-10-03 14:34:09 -0300

    Criando o HTML e css da página de erro

--- Back-end/app.js
+++ Back-end/app.js
@@ -4,7 +4,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-
+//rotas
 const indexRouter = require('./routes/index')
 const carrinhoRouter = require('./routes/carrinho')
 const confirmacaoDePagamentoRouter = require('./routes/confirmacaoDePagamento')
