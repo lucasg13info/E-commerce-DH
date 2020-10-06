98149f0c1b856f0136ed95b5735d286473a03fe7
juangpu <juangabriel.urrutia@outlook.com>
2020-10-05 22:54:59 -0300

    Atualizanddo index

--- Back-end/controllers/indexController.js
+++ Back-end/controllers/indexController.js
@@ -5,9 +5,9 @@ const {Products} = require ('../models');
 module.exports = {
 
     index: async (req, res) =>{
-        let produtos = await Products.findAll({include: 'typeProducts'});
-        res.send(produtos);
+        // let produtos = await Products.findAll({include: 'typeProducts'});
+        // res.send(produtos);
 
-    //    res.render('index')
+       res.render('index')
     }
 }
