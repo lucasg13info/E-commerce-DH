e66e8e2ecf097f024775b2e621636cda376c7c7b
Lucas Estefano <lucccasestefano_@hotmail.com>
2020-10-05 21:10:45 -0300

    Migrations

---
+++ Back-end/migrations/20201005234115-products.js
@@ -0,0 +1,44 @@
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('products', {
+      prd_id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      prd_descricao: {
+        type: Sequelize.STRING
+      },
+      prd_status: {
+        type: Sequelize.INTEGER
+      },
+      prd_valor: {
+        type: Sequelize.INTEGER
+      }, 
+      qtd_estoque: {
+        type: Sequelize.INTEGER, 
+        allowNull: true,
+      },
+      
+       type_produts_type_id: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: "typeProducts",
+          key: "type_id"
+        }
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('products');
+  }
+};
---
+++ Back-end/models/products.js
@@ -0,0 +1,43 @@
+const { Model } = require('sequelize');
+  module.exports = (sequelize, DataTypes) => {
+   
+    const Products = sequelize.define('Products', {
+      // definindo os atributos do model!
+      prd_id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: DataTypes.INTEGER
+      },
+      prd_descricao: DataTypes.STRING,
+      prd_status: DataTypes.INTEGER,
+      prd_valor: DataTypes.INTEGER,
+      qtd_estoque: {
+        type: DataTypes.INTEGER, 
+        allowNull: true,
+      },
+      
+      
+      type_produts_type_id: {
+        type: DataTypes.INTEGER,
+        references: {
+          model: "typeProducts",
+          key: "type_id"
+        }
+      }
+    }, {
+      // definindo opções do model!
+      sequelize,
+      modelName: 'Products',
+      tableName: 'products '
+    });
+    
+    Products.associate = (models)=> {
+      //associações vão aqui!
+      Products.belongsTo(models.typeProducts, {
+        foreingKey: "type_produts_type_id"
+      })
+    } 
+    
+    return Products;
+  };
