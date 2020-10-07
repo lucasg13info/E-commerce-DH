cc31380a310f0369b73ea20cc4b8e248e82e5e16
juangpu <juangabriel.urrutia@outlook.com>
2020-10-07 13:02:55 -0300

    Criando Migration e Model da tabela picture_product do banco

---
+++ Back-end/migrations/20201007155905-pictureProduct.js
@@ -0,0 +1,37 @@
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('pictureProducts', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      pic_carrinho: {
+        type: Sequelize.STRING
+      },
+      pic_status: {
+        type: Sequelize.INTEGER
+      },
+    
+      products_id: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: "products",
+          key: "id"
+        }
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('pictureProducts');
+  }
+};
---
+++ Back-end/models/pictureProduct.js
@@ -0,0 +1,39 @@
+const { Model } = require('sequelize');
+  module.exports = (sequelize, DataTypes) => {
+   
+    const pictureProduct = sequelize.define('pictureProduct', {
+      // definindo os atributos do model!
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: DataTypes.INTEGER
+      },
+      pic_carrinho: DataTypes.STRING,
+      pic_status: DataTypes.INTEGER,      
+      
+      products_id: {
+        type: DataTypes.INTEGER,
+        references: {
+          model: "products",
+          key: "id"
+        },
+        // field: "type_produts_id"
+      }
+    }, {
+      // definindo opções do model!
+      sequelize,
+      modelName: 'pictureProduct',
+      tableName: 'picture_product'
+    });
+    
+    // Products.associate = (models)=> {
+    //   //associações vão aqui!
+    //   Products.belongsTo(models.type_products, {
+    //     foreingKey: "type_produts_id",
+    //     targetKey: "id"
+    //   })
+    // } 
+    
+    return Products;
+  };
