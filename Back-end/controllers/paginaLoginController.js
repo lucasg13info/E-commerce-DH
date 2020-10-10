8f4354a6a1794ea3ceac70b77c57cb4ec4e359a3
Lucas Estefano <lucccasestefano_@hotmail.com>
2020-10-09 22:34:13 -0300

    Ajustes no form de login

--- Back-end/controllers/paginaLoginController.js
+++ Back-end/controllers/paginaLoginController.js
@@ -1,5 +1,24 @@
+const {users} = require ('../models')
+
+
 module.exports  = {
     paginaLogin: (req,res) =>{
         res.render('PaginaLogin')
+    },
+    store: async (req, res) => {
+        const {
+            name,
+            email,
+            password
+        } = req.body 
+
+        const resultado = await users.create({
+            name,
+            email,
+            password
+        })
+        console.log(resultado)
+
+        return res.redirect('/')
     }
 };
