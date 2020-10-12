e778b93ef96558b3423b88561532943fb80515c3
Lucas Estefano <lucccasestefano_@hotmail.com>
2020-10-11 23:55:33 -0300

    AJuste

--- Back-end/config/database.js
+++ Back-end/config/database.js
@@ -1,10 +1,6 @@
 module.exports = {
     username: 'root',
-<<<<<<< HEAD
     password: 'infantaria0729',
-=======
-    password: 'root',
->>>>>>> 3a5239ac32a288ffe8fad74325ebb6518fea0920
     database: 'echopet',
     host: '127.0.0.1',
     dialect: 'mysql',
