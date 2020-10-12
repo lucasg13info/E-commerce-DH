2edf3a3142a258ed4e8248ca4221d83da4ccbef7
Lucas Estefano <lucccasestefano_@hotmail.com>
2020-10-12 03:50:52 -0300

    Concluindo a tela confirmação de pagamento e ajustes na responsividade do header

--- Back-end/routes/crudProduto.js
+++ Back-end/routes/crudProduto.js
@@ -1,17 +1,28 @@
-var express = require('express');
-var router = express.Router();
-const crudProdutoController = require('../controllers/crudProdutoController');
+// var express = require('express');
+// var router = express.Router();
+// const crudProdutoController = require('../controllers/crudProdutoController');
 
-/* GET users listing. */
-router.get('/produtos', crudProdutoController.index);
-router.get('/ver/:id', crudProdutoController.findById);
-router.get('/pesquisar', crudProdutoController.search);
+// /* GET users listing. */
+// router.get('/produtos', crudProdutoController.index);
+// router.get('/ver/:id', crudProdutoController.findById);
+// router.get('/pesquisar', crudProdutoController.search);
 
-router.get('/cadastro', crudProdutoController.create);
-router.post('/cadastro', crudProdutoController.store);
+// router.get('/cadastro', crudProdutoController.create);
+// router.post('/cadastro', crudProdutoController.store);
 
-router.get('/editar/:id', crudProdutoController.edit);
-router.put('/editar/:id', crudProdutoController.update);
-router.delete('/deletar/:id', crudProdutoController.destroy);
+// router.get('/editar/:id', crudProdutoController.edit);
+// router.put('/editar/:id', crudProdutoController.update);
+// router.delete('/deletar/:id', crudProdutoController.destroy);
+
+// module.exports = router;
+
+
+
+const express = require('express');
+const router = express.Router();
+const crudController = require ('../controllers/crudProdutoController');
+
+router.get('/', crudController.crudProduto);
+
 
 module.exports = router;
