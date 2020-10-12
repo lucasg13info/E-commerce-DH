a7e2c4ea30a89e39b4219741db05e2df8de9e25e
juangpu <juangabriel.urrutia@outlook.com>
2020-10-11 21:47:59 -0300

    Configurando barra de busca no header

--- Back-end/routes/index.js
+++ Back-end/routes/index.js
@@ -4,6 +4,7 @@ const indexController = require('../controllers/indexController')
 
 /* GET home page. */
 router.get('/', indexController.index); 
+router.get('/search', indexController.search);
 
 // CADASTRAR PRODUTO - ROTA
 // router.get('/cadastrarProduto', indexController.bulkCreate);
@@ -11,7 +12,5 @@ router.get('/', indexController.index);
 
 // Criar View com formulario de cadstro de produto apenas para os devs efetuarem cadastro de materiais.
 
-
-// router.get('/search', indexController.search);
 
 module.exports = router;
