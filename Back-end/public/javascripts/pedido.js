b945d34e1e29089507ae4a0690b06c7b7f4f738b
GuMunhoz <gustavo_munhoz@yahoo.com>
2020-10-12 16:57:36 -0300

    Alterando fluxo de finalização de pedido

---
+++ Back-end/public/javascripts/pedido.js
@@ -0,0 +1,9 @@
+const pedido = document.querySelector(".login-confirmar");
+
+pedido.onclick = function(){
+    let num = Math.floor(Math.random()*456789);
+    alert("Compra efetuada!, seu número de pedido é : "+ num);
+    
+
+}
+
