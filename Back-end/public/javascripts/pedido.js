486b593e0152e5e40cc3411d08ba5c5f0566547b
juangpu <juangabriel.urrutia@outlook.com>
2020-10-13 19:12:44 -0300

    Adicionando Swal ao gerar o número do pedido - teste

--- Back-end/public/javascripts/pedido.js
+++ Back-end/public/javascripts/pedido.js
@@ -3,7 +3,7 @@ const pedido = document.querySelector(".login-confirmar");
 pedido.onclick = function(){
     let num = Math.floor(Math.random()*456789);
     alert("Compra efetuada!, seu número de pedido é : "+ num);
-    
+    // swal("Compra efetuada!", "Seu numero de pedido é: "+ num, "success");
 
 }
 
