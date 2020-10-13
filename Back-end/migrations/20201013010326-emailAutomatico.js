a086b55d56a24e37667f109fb69044fbca2b6f47
juangpu <juangabriel.urrutia@outlook.com>
2020-10-12 22:07:34 -0300

    Criando Model e Migration para receber e-mails e fazer envio automatico de um e-mail padrão para o user

---
+++ Back-end/migrations/20201013010326-emailAutomatico.js
@@ -0,0 +1,26 @@
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Email', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      emailAutomatico: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Email');
+  }
+};
