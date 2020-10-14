80513116445d9b4a17c853dc6e282438239e9de2
GuMunhoz <gustavo_munhoz@yahoo.com>
2020-10-13 23:14:14 -0300

    criação do numero de pedido

--- Back-end/public/javascripts/pedido.js
+++ Back-end/public/javascripts/pedido.js
@@ -1,9 +1,18 @@
+
+
 const pedido = document.querySelector(".login-confirmar");
 
 pedido.onclick = function(){
     let num = Math.floor(Math.random()*456789);
-    alert("Compra efetuada!, seu número de pedido é : "+ num);
+    swal({
+        title: "Compra efetuada!",
+        text: "Seu número de pedido é " + num +"!",
+        icon: "success",
+        button: "Continuar",
+      });
     
 
-}
+};
+
+
 
