c2f236e138128245cb8d99e0432329df5d6f8e48
juangpu <juangabriel.urrutia@outlook.com>
2020-10-15 18:34:04 -0300

    Ajustando cupom de desconto, usando JS para fornt no arquivo carrinho.js finalizando SWAL validando o cupom PETISCAO, ajustando as views com o nome do cupom

--- Back-end/public/javascripts/carrinho.js
+++ Back-end/public/javascripts/carrinho.js
@@ -27,11 +27,29 @@ window.addEventListener('load', function() {
                 content: "input",
               })
               .then((value) => {
-                swal(`Cupom de desconto: ${value}`);        
+                // swal(`Cupom de desconto: ${value}`);   
+                switch(value){
+                    case "PETISCAO" :
+                        return swal("Cupom Valido!", "Com o cupom PETISCAO você tem R$ 10,00 de desconto", "success");
+                        break;
+                    case null :
+                        return swal("Cupom Invalido!", "Cupom vazio.", "error");
+                    default: swal("Cupom Invalido!", `O cupom ${value} não existe`, "error");
+                }  
+
     });
-});   
+}, );   
 
 
 });
 
 // swal("Cupom Valido!", "O cupom PETISCAO é valido!", "success")
+
+// function cupom() {
+    // switch(){
+    //     case "PESTISCAO" :
+    //         return swal("Cupom Valido!", "O cupom PETISCAO é valido!", "success");
+    //         break;
+    //     default: swal("Cupom Invalido!", "Esse cupom é inválido!", "error");
+    // }
+// }
