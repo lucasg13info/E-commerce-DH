947d9320146dc9263c5d35ea1f3b236fdd003037
juangpu <juangabriel.urrutia@outlook.com>
2020-10-15 13:39:38 -0300

    Adiconando SWAL no cupom de desconto do carrinho com o arquivo carrinho.js a pasta public

--- Back-end/public/javascripts/carrinho.js
+++ Back-end/public/javascripts/carrinho.js
@@ -4,3 +4,34 @@
 //     }
 
 //     console.log(somaCarrinho);
+
+// let nome = swal("Digite seu cupom:", {
+//     content: "input",
+//   })
+//   .then((value) => {
+//     swal(`cupom: ${value}`);
+//   });
+
+// alert('Olá, ' + nome);
+// swal("Cupom Valido!", "O cupom PETISCAO é valido!", "success");
+
+// document.getElementById('cupomDesconto').innerText += ' ' + nome;
+
+//
+
+window.addEventListener('load', function() {
+
+    let linkContato = document.getElementById('cupomDesconto');
+        linkContato.addEventListener("click", function() {
+            swal("Digite seu cupom:", {
+                content: "input",
+              })
+              .then((value) => {
+                swal(`Cupom de desconto: ${value}`);        
+    });
+});   
+
+
+});
+
+// swal("Cupom Valido!", "O cupom PETISCAO é valido!", "success")
