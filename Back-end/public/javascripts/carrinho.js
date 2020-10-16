e4fb836eac62fb2ad9a9c7e8a471fd71e28ce5da
juangpu <juangabriel.urrutia@outlook.com>
2020-10-16 01:33:37 -0300

    Cadastrando produtos via CRUD para rodar o forEach

--- Back-end/public/javascripts/carrinho.js
+++ Back-end/public/javascripts/carrinho.js
@@ -19,29 +19,29 @@
 
 // CUPOM DE DESCONTO
 
-// window.addEventListener('load', function() {
+window.addEventListener('load', function() {
 
-//     let linkContato = document.getElementById('cupomDesconto');
-//         linkContato.addEventListener("click", function() {
-//             swal("Digite seu cupom:", {
-//                 content: "input",
-//               })
-//               .then((value) => {
-                // swal(`Cupom de desconto: ${value}`);   
-//                 switch(value){
-//                     case "PETISCAO" :
-//                         return swal("Cupom Valido!", "Com o cupom PETISCAO você tem R$ 10,00 de desconto", "success");
-//                         break;
-//                     case null :
-//                         return swal("Cupom Invalido!", "Cupom vazio.", "error");
-//                     default: swal("Cupom Invalido!", `O cupom ${value} não existe`, "error");
-//                 }  
+    let linkContato = document.getElementById('cupomDesconto');
+        linkContato.addEventListener("click", function() {
+            swal("Digite seu cupom:", {
+                content: "input",
+              })
+              .then((value) => {
+                swal(`Cupom de desconto: ${value}`);   
+                switch(value){
+                    case "PETISCAO" :
+                        return swal("Cupom Valido!", "Com o cupom PETISCAO você tem R$ 10,00 de desconto", "success");
+                        break;
+                    case null :
+                        return swal("Cupom Invalido!", "Cupom vazio.", "error");
+                    default: swal("Cupom Invalido!", `O cupom ${value} não existe`, "error");
+                }  
 
-//     });
-// }, );   
+    });
+}, );   
 
 
-// });
+});
 
 // swal("Cupom Valido!", "O cupom PETISCAO é valido!", "success")
 
